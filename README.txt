2977e26b3fb2043bb78acc07e09c98134a1aa3c2
wvcneto <wvcneto@gmail.com>
2020-06-18 20:57:43 -0300

    React/Axios comunication with API(gostack-n1)

---
+++ README.txt
@@ -0,0 +1,43 @@
+﻿<h4 align="center">
+  Challenge N2
+</h4>
+
+<p align="center">
+  Technologies
+</p>
+
+## :rocket: Technologies
+
+This project was developed at the [RocketSeat GoStack Bootcamp](https://rocketseat.com.br/bootcamp) with the following technologies:
+
+-  [Node.js][nodejs]
+-  [ReactJS](https://reactjs.org/)
+-  [Axios](https://github.com/axios/axios)
+-  [VS Code][vc] with [ESLint][vceslint]
+
+---
+
+## Install Dependencies.
+
+```bash
+yarn
+```
+_(During development, Yarn was used as a dependency manager)_
+
+---
+
+## Run
+
+```bash
+yarn start
+```
+
+---
+
+By Walter Neto (https://www.linkedin.com/in/wvcneto)
+
+[nodejs]: https://nodejs.org/
+[yarn]: https://yarnpkg.com/
+[vc]: https://code.visualstudio.com/
+[vceditconfig]: https://marketplace.visualstudio.com/items?itemName=EditorConfig.EditorConfig
+[vceslint]: https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint
--- src/App.js
+++ src/App.js
@@ -1,28 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
+import { uuid } from 'uuidv4';
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    async function loadResource(){
+      const response = await api.get('/repositories');
+
+      setRepositories(response.data);
+    }
+
+    loadResource();
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      id: uuid(),
+      title: `New Repository ${Date.now()}`,
+      url: 'http://github.com',
+      techs: ["NodeJS", "ReactJS"],
+      likes: 0
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`repositories/${id}`);
+    
+    if(response.status === 204) {
+      const newRepositories = repositories.filter(
+        repository => repository.id !== id
+      );
+
+      setRepositories(newRepositories);
+    } else {
+      alert('Não foi possível excluir o repositório');
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+          {
+            repositories.map(repository => <li key={repository.id}>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>)
+          }          
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
